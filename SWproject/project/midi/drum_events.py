b6ca3e3dcc8278a398568d6d4c9a362ea0b51b03
mame623-arch <melter623@gmail.com>
2025-11-26 19:15:09 +0900

    Modify play_drum to include start_offset parameter

--- SWproject/project/midi/drum_events.py
+++ SWproject/project/midi/drum_events.py
@@ -8,7 +8,7 @@ def playOn(track: MidiTrack, note: int, time: int, channel: int):
 def playOff(track: MidiTrack, note: int, time: int, channel: int):
     track.append(Message('note_off', note=note, velocity=64, time=time, channel=channel))
 
-def play_drum(track: MidiTrack, pattern: dict):
+def play_drum(track: MidiTrack, pattern: dict, start_offset: int = 0):
     # 드럼 리듬 패턴을 MIDI 트랙에 기록
 
     for step in range(16):  # 한 마디 = 16 step (16분음표)
@@ -17,11 +17,14 @@ def play_drum(track: MidiTrack, pattern: dict):
             if sequence[step] == 1:
                 active_notes.append(DRUM_NOTES[name])
 
+        time = NOTE_LENGTH["SIXTEENTH"]
+        if step == 0:
+            time = start_offset if start_offset > 0 else NOTE_LENGTH["SIXTEENTH"]
+
         if active_notes:
-            for note in active_notes: # 한 악기만 연주할 경우
-                playOn(track, note, time=0, channel=DRUM_CHANNEL)
-            for i, note in enumerate(active_notes): # 동시에 연주되는 음 처리
+            for i, note in enumerate(active_notes):
+                playOn(track, note, time=time if i == 0 else 0, channel=DRUM_CHANNEL)
+            for i, note in enumerate(active_notes):
                 playOff(track, note, time=NOTE_LENGTH["SIXTEENTH"] if i == 0 else 0, channel=DRUM_CHANNEL)
-        else:
-            # 쉼표 구간이면 시간만 흐르게 함
-            track.append(Message('note_off', note=35, velocity=0, time=NOTE_LENGTH["SIXTEENTH"], channel=DRUM_CHANNEL))
+        else: # 쉼표 구간이면 시간만 흐르게 함
+            track.append(Message('note_off', note=35, velocity=0, time=time, channel=DRUM_CHANNEL))
